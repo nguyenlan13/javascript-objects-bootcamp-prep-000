8241e904bcb32030dc479dfb876e32c24bf51986
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-11 11:28:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,9 +2,7 @@
 
 var playlist = {artistName: 'songTitle'};
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist ['Slowdive'] = 'Alison';
-  playlist ['My Bloody Valentine'] = 'Sometimes';
-  playlist ['Phil Ochs'] = "Here's to the State of Mississippi";
-  
+  playlist [artistName] = 'songTitle';
+
  
 }
