d90d48a9c061b4fc490d763ba35bc013414cf66a
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-11 11:27:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 
 
-var playlist = {"Above and Beyond": 'sticky fingers'};
+var playlist = {artistName: 'songTitle'};
 function updatePlaylist(playlist, artistName, songTitle){
   playlist ['Slowdive'] = 'Alison';
   playlist ['My Bloody Valentine'] = 'Sometimes';
